28acc67e726b7b62c29294f007efe8cfafa2abd3
Sora-bs <aozora@icloud.com>
2019-07-16 01:52:48 +0900

    Initial Commit

---
+++ todoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todoApp
+//
+//  Created by sora on 2019/07/16.
+//  Copyright © 2019 sora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
